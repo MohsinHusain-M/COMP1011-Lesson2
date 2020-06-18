47eac6bd9afc7f1678f10cab8e5ba5ec91d0e5c3
BlackArc <BlackArc@DESKTOP-HSHH8LK>
2020-06-18 03:12:02 -0700

    Just made

---
+++ COMP1011-Lesson2/src/Hero.java
@@ -0,0 +1,20 @@
+
+public class Hero {
+	int strength;
+	int speed;
+	int health;
+	String name;
+	public Hero(String name) {
+		this.name = name;
+	}
+	
+	private void generateAbilities() {
+		
+	}
+	public void fight() {
+		
+	}
+	public void run() {
+		
+	}
+}
