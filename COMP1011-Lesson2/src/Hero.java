90062cced685b8693332ff1d82ec2493af171829
BlackArc <BlackArc@DESKTOP-HSHH8LK>
2020-06-18 03:37:23 -0700

    Updated Hero class. Flushed all the methods.

--- COMP1011-Lesson2/src/Hero.java
+++ COMP1011-Lesson2/src/Hero.java
@@ -25,16 +25,25 @@ public class Hero {
 		this.name = name;
 	}
 	
+	//PUBLIC METHOD ++++++++++++++++++++++++++++++++
+	public void showAbilities() {
+		System.out.println("+++++++++++++++++++++++++++");
+		System.out.println("Strength " + this.strength);
+		System.out.println("Speed " + this.speed);
+		System.out.println("Health " + this.health);
+	}
 	
 	//PRIVATE METHOD +++++++++++++++++++++++++++++++
 	private void generateAbilities() {
-		
+		this.strength = (int) Math.random()*100+1;
+		this.speed = (int) Math.random()*100+1;
+		this.health = (int) Math.random()*100+1;
 	}
 	public void fight() {
-		
+		System.out.println(this.name + " is fighting.");
 	}
 	public void run() {
-		
+		System.out.println(this.name +" is Running");
 	}
 
 
